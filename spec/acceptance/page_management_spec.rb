75a27a9a786a1548fcd4a38ecfb52d9f314edc27
Patrik Stenmark <patrik.stenmark@gmail.com>
2010-06-13 17:03:52 +0200

    rspec matcher to own file

--- spec/acceptance/page_management_spec.rb
+++ spec/acceptance/page_management_spec.rb
@@ -74,20 +74,4 @@ feature "page management" do
     
     page.should_not have_content("Om datasektionen")
   end
-  
-  RSpec::Matchers.define :have_success_message do
-    match do |page|
-      page.should have_css("div#flash_notice")
-    end
-    
-    failure_message_for_should do |actual|
-      "expected that the page should contain a success message, but it didn't"
-    end
-
-    failure_message_for_should_not do |actual|
-      "expected that the page should not contain a sucess message, but it did"
-    end
-  end
-  
-  
 end
