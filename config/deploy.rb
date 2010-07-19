853b69d8269c41c2b87c0d79a870e113c1bbf45c
Patrik Stenmark <patrik.stenmark@gmail.com>
2010-07-19 14:43:41 +0200

    make migrate work

--- config/deploy.rb
+++ config/deploy.rb
@@ -88,7 +88,7 @@ namespace :bundler do
  
   task :bundle_new_release, :roles => :app do
     bundler.create_symlink
-    run "cd #{release_path} && bundle install --without test"
+    run "cd #{release_path} && bundle install"
   end
 end
  
