9f58c1b06f8c27825ba5f2fb0a1b045213bd4b42
Joakim Ekberg <jocke.ekberg@gmail.com>
2010-07-25 18:28:40 +0200

    Tweaking of the information popup.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   $(".news-item").each(function() {
     var newsItem = $(this);
-    $(this).find(".thumb").mouseover(function() {
+
+    newsItem.find(".thumb").mouseover(function() {
       newsItem.find(".popup").show();
     });
     
-    $(this).find(".thumb").mouseout(function() {
+    newsItem.find(".thumb").mouseout(function() {
       newsItem.find(".popup").hide();
     });
   });
