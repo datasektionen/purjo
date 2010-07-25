20be80f09bb5257de8f26899c821c605a2550e7c
Patrik Stenmark <patrik.stenmark@gmail.com>
2010-07-25 11:26:23 +0200

    fix cv database

--- app/helpers/cvs_helper.rb
+++ app/helpers/cvs_helper.rb
@@ -1,14 +1,15 @@
 module CvsHelper
   def show(cv, name, method)
     result = cv.send(method)
-    
+    output = ""
     unless result.blank?
-      output = "<p><h3>#{name}</h3></p>\n"
-      output += "<p>#{cv.send(method)}</p>\n"
+      output += "<p><h3>#{name}</h3></p>\n"
+      output += "<p>#{textilize(cv.send(method).to_s)}</p>\n"
     end
+    output.html_safe
   end
   
   def cv_language_links
-    Cv::LANGUAGES.map { |language, abbr| link_to language, cvs_path(:language => abbr) }.join(" | ")
+    Cv::LANGUAGES.map { |language, abbr| link_to language, cvs_path(:language => abbr) }.join(" | ").html_safe
   end
 end
