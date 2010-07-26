2ddaecbcf5ba12dfeaabead9f414e98a6e23b983
Victor Hallberg <vigge19@gmail.com>
2010-07-26 14:24:41 +0200

    added edit functionality to newsletter sections

--- app/controllers/newsletters/newsletter_sections_controller.rb
+++ app/controllers/newsletters/newsletter_sections_controller.rb
@@ -1,8 +1,17 @@
 class NewsletterSectionsController < InheritedResources::Base
+  require_role :admin, :only => [:edit, :update, :delete]
   require_role :editor
   
   belongs_to :newsletter
-  
+
+  # TODO: varför vill inte InheritedResources göra det här åt mig? =(
+  def edit
+  end
+
+  def update
+    update! { newsletter_path(@newsletter) }
+  end
+
   def create
     create! { newsletter_path(@newsletter) }
   end
