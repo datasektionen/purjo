b4cb5d8ad4ca0b02331e4d5658c7142f6766b66c
Stefan Berggren <sbergg@kth.se>
2010-07-27 09:40:51 +0200

    "Aktiverade" funk&valsystemet.
    "Aktiverade" val- och funktionärrsystemet samt tog bort lite kod som inte ville funka med rais 3, planen är att få det att funka igen innan release.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do |map|
-  
+
   resources :blogs do
     resources :articles
   end
@@ -75,22 +75,21 @@ Rails.application.routes.draw do |map|
   map.schema '/schema/proxy.:format', :controller => 'schema', :action => 'proxy'
   map.schema '/schema/:year', :controller => 'schema', :action => 'index', :year => 'D1'
 
+  # Måste ligga ovanför alla resurser som använder auto_complete:
+  map.auto_complete ':controller/:action',
+    :requirements => { :action => /auto_complete_for_\S+/ },
+    :conditions => { :method => :get }
+
+  map.resources :nominees, :as => "sektionen/val"
+
+  map.resources :election_events
+
+  map.resources :chapter_posts
+
+  map.resources :functionaries, :as => "sektionen/funktionarer"
+
   map.root :controller => 'front_pages', :action => 'show'
 
   map.connect "*url", :controller => 'nodes', :action => 'show'
+
 end
-
-# Illaluktande kommentarer
-#  # Måste ligga ovanför alla resurser som använder auto_complete:
-#  map.auto_complete ':controller/:action',
-#    :requirements => { :action => /auto_complete_for_\S+/ },
-#    :conditions => { :method => :get }
-#
-#  map.resources :nominees, :as => "sektionen/val"
-#
-#  map.resources :election_events
-#
-#  map.resources :chapter_posts
-#
-#  map.resources :functionaries, :as => "sektionen/funktionarer"
-
