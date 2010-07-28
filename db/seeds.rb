e380b37da47a8ec772d941b1c0c744ac242fa6a7
Patrik Stenmark <patrik.stenmark@gmail.com>
2010-07-28 17:58:43 +0200

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+root_page = TextNode.create!(:name => 'root', :url => '/', :contents => "I is root")
+
+person_info_file = Rails.root + "config/my_user.yml"
+raise "Skapa en config/my_user.yml (exempel finns i katalogen)" unless File.exists?(person_info_file)
+
+person_info = YAML::load(File.read(person_info_file))
+person = Person.create!(person_info)
+
