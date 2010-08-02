8b7409ba39a7baf3adc824a9d86f5ffe98e3942b
Stefan Berggren <sbergg@kth.se>
2010-08-02 13:10:18 +0200

    Tog bort lite länkar icke-editors inte borde se.
    Tog även bort lite gammalt som låg och skräpade i routes.

--- config/routes.rb
+++ config/routes.rb
@@ -75,11 +75,6 @@ Rails.application.routes.draw do |map|
   map.schema '/schema/proxy.:format', :controller => 'schema', :action => 'proxy'
   map.schema '/schema/:year', :controller => 'schema', :action => 'index', :year => 'D1'
 
-  # Måste ligga ovanför alla resurser som använder auto_complete:
-  map.auto_complete ':controller/:action',
-    :requirements => { :action => /auto_complete_for_\S+/ },
-    :conditions => { :method => :get }
-
   map.resources :nominees, :as => "sektionen/val"
 
   map.resources :election_events
