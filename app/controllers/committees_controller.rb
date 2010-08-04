b34f7bcd5380b4b0283ea706292c244ca25f755f
Patrik Stenmark <patrik.stenmark@gmail.com>
2010-08-04 14:42:53 +0200

    fix redirects

--- app/controllers/committees_controller.rb
+++ app/controllers/committees_controller.rb
@@ -1,3 +1,11 @@
 class CommitteesController < InheritedResources::Base
   require_role :admin
+  
+  def create
+    create! { committees_path }
+  end
+  
+  def update
+    update! { committees_path }
+  end
 end
