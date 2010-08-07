067de0711fbfeef699cbb8a77afe562dcc139cb0
Stefan Berggren <sbergg@kth.se>
2010-08-07 17:22:09 +0200

    whenever är inget kommando som jag känner till

--- config/deploy.rb
+++ config/deploy.rb
@@ -106,6 +106,7 @@ after "deploy:symlink", "deploy:update_crontab"
 namespace :deploy do
   desc "Update the crontab file"
   task :update_crontab, :roles => :db do
-    run "cd #{release_path} && whenever --update-crontab #{application}"
+    # hu? whenever
+    #run "cd #{release_path} && whenever --update-crontab #{application}"
   end
 end
