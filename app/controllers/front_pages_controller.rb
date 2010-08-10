a7130b912f97d96a3efd0375e5c558ef1fe3c01b
Patrik Stenmark <patrik.stenmark@gmail.com>
2010-08-10 20:40:15 +0200

    sort tags

--- app/controllers/front_pages_controller.rb
+++ app/controllers/front_pages_controller.rb
@@ -4,7 +4,7 @@ class FrontPagesController < ApplicationController
   def show
     @news_posts = Post.news_posts
     
-    @tags = ActsAsTaggableOn::Tag.all
+    @tags = ActsAsTaggableOn::Tag.all.sort { |t1, t2| t2.taggings.count <=> t1.taggings.count }
     if params[:tags]
       @news_posts = @news_posts.tagged_with(params[:tags])
       @active_tags = ActsAsTaggableOn::TagList.from(params[:tags])
