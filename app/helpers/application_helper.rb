aa8365675d5691d49c9bbda3c9f3cc0dfc0dd594
Stefan Berggren <sbergg@kth.se>
2010-08-22 20:56:29 +0200

    Förbättrade side_link så den funkar (bättre).
    Ändrade även namnet på Funktioner i menyn till Länkar, fortfarande inte ett bra namn.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,8 +38,16 @@ module ApplicationHelper
     end
   end
   
-  def side_link(title, url, *options)
+  def side_link(*options)
     @view_menus = Array.new unless @view_menus
-    @view_menus.push :title => title, :url => url
+    @view_menus.push options
+  end
+  
+  def get_side_link(link)
+    if link[2]
+      link_to link[0], link[1], link[2]
+    else
+      link_to link[0], link[1], link[2]
+    end
   end
 end
