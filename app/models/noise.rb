f028243fb6ca24f817b018a49f0c827f40d7c8b9
Victor Hallberg <vigge19@gmail.com>
2010-09-10 14:06:49 +0200

    fixed comments display + now require message

--- app/models/noise.rb
+++ app/models/noise.rb
@@ -2,6 +2,8 @@ class Noise < ActiveRecord::Base
 	belongs_to :post
 	belongs_to :person
 
+  validates_length_of :message, :minimum => 2, :too_short => "Du måste ange ett meddelande"
+
   def editable?
     ((Time.now-created_at < 10.minutes) and (Person.current.id == person_id)) or Person.current.editor?
   end
