9461dcf486f3b7ccc640788229cd4f8af0a12008
Victor Hallberg <vigge19@gmail.com>
2010-11-10 14:55:38 +0100

    i18n for search results

--- app/sweepers/calendar_sweeper.rb
+++ app/sweepers/calendar_sweeper.rb
@@ -2,6 +2,5 @@ class CalendarSweeper < ActionController::Caching::Sweeper
   observe Post
   def after_save(post)
     Rails.cache.delete("views/front_pages/agenda")
-    Rails.cache.delete("views/front_pages/tag_list")
   end
 end
