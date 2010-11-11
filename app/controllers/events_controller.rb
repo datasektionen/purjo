0c98d464c285351b0085385eef7b87ec12e13268
Victor Hallberg <vigge19@gmail.com>
2010-11-11 22:30:04 +0100

    added admin link to create event for posts

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,6 +6,14 @@ class EventsController < InheritedResources::Base
     now = DateTime.now.utc
     @event.starts_at = DateTime.new(now.year, now.month, now.day, now.hour, 0, 0)
     @event.ends_at = @event.starts_at + 1.hour
+
+    if params[:post]
+      @post = Post.find(params[:post])
+      unless @post.nil?
+        @event.name ||= @post.name
+        @event.content ||= "*Relaterad nyhet:* \"#{@post.name}\":/nyheter/#{@post.id}" 
+      end
+    end
   end
   
   def create
