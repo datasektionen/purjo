1547dce050ef3be1ceb8fb5be623d4fbd1715747
Niklas Pedersen <niklas.pedersen@gmail.com>
2010-11-12 11:42:52 +0100

    Fix for All day events and possible fix for timezone in google calendar

--- lib/ior/calendar/ics_renderer.rb
+++ lib/ior/calendar/ics_renderer.rb
@@ -10,14 +10,15 @@ module Ior
         output += "VERSION:2.0\r\n"
         output += "X-WR-CALNAME:Kongl. Datasektionen\r\n"
         output += "PRODID:-//hacksw/handcal//NONSGML v1.0//EN\r\n"
+        output += "X-WR-TIMEZONE:Europe/Stockholm\r\n"
         output += "TZID:Europe/Stockholm\r\n"
         
         @events.each do |event|
           output += "BEGIN:VEVENT\r\n"
           
-          if event.all_day
-            output += "DTSTART:#{(event.starts_at).strftime("%Y%m%d")}T000000\r\n"
-            output += "DTEND:#{(event.ends_at).strftime("%Y%m%d")}T235959\r\n"
+          if event.all_day || (event.ends_at - event.starts_at) >= 24.hours
+            output += "DTSTART;VALUE=DATE:#{(event.starts_at).strftime("%Y%m%d")}\r\n"
+            output += "DTEND;VALUE=DATE:#{(event.ends_at).strftime("%Y%m%d")}\r\n"
           else
             output += "DTSTART:#{event.starts_at.strftime("%Y%m%dT%H%M%S")}\r\n"
             output += "DTEND:#{event.ends_at.strftime("%Y%m%dT%H%M%S")}\r\n"
