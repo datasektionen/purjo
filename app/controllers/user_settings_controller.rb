67d17151755fdfaa2bf24816f89437b59a778da2
Victor Hallberg <vigge19@gmail.com>
2010-11-16 13:29:19 +0100

    still not working, reverting

--- app/controllers/user_settings_controller.rb
+++ app/controllers/user_settings_controller.rb
@@ -1,28 +1,25 @@
 class UserSettingsController < ApplicationController
   before_filter :load_person
+  before_filter :check_access
   
   def edit_current
-    raise IOR::Security::AccessDenied unless authorized?
     @person = Person.current
     @user_settings = @person.user_settings
     render :action => 'edit'
   end
 
   def show
-    raise IOR::Security::AccessDenied unless authorized?
     @user_settings = @person.user_settings
     render :action => 'edit'
   end
   
   def create
-    raise IOR::Security::AccessDenied unless authorized?
     @user_settings = @person.build_user_settings(params[:user_settings])
     @user_settings.save!
     redirect_to 
   end
   
   def update
-    raise IOR::Security::AccessDenied unless authorized?
     @user_settings = @person.user_settings
     @user_settings.update_attributes(params[:user_settings])
     @user_settings.save!
@@ -34,7 +31,9 @@ class UserSettingsController < ApplicationController
     @person = Person.find_by_kth_username(params[:person_id])
   end
   
-  def authorized?
-    Person.current.admin? || Person.current == @person
+  def check_access
+    if !(Person.current.admin? || Person.current == @person)
+      raise IOR::Security::AccessDenied
+    end
   end
 end
