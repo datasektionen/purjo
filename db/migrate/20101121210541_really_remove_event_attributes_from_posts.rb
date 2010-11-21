dc2946bea1b1d9af92faa4901b8a8949de351063
Victor Hallberg <vigge19@gmail.com>
2010-11-22 00:01:28 +0100

    real post event attribute removal migration

---
+++ db/migrate/20101121210541_really_remove_event_attributes_from_posts.rb
@@ -0,0 +1,21 @@
+class ReallyRemoveEventAttributesFromPosts < ActiveRecord::Migration
+  def self.up
+    change_table :posts do |t|
+      t.remove :news_post
+      t.remove :calendar_post
+      t.remove :starts_at
+      t.remove :ends_at
+      t.remove :all_day
+    end
+  end
+
+  def self.down
+    change_table :posts do |t|
+      t.boolean  "news_post",     :default => false
+      t.boolean  "calendar_post", :default => false
+      t.datetime "starts_at"
+      t.datetime "ends_at"
+      t.boolean  "all_day"
+    end
+  end
+end
