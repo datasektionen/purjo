7dfbbade5b89ef190caf0b10481b32609fde9b3a
Victor Hallberg <vigge19@gmail.com>
2010-12-03 16:23:57 +0100

    reverted post searching order/scoping

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,8 +16,6 @@ class Post < ActiveRecord::Base
   validates_presence_of :name
   
   searchable do
-    with(:published_at).less_than(Time.now)
-    order_by(:published_at, :desc)
     text :content
     text :name, :default_boost => 2
     string :tags, :multiple => true, :using => :categories
