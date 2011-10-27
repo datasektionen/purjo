8a4a4789ff892282c869bafc66d8f10136b208b4
Martin Frost <frost@ceri.se>
2011-10-27 15:50:37 +0200

    Bumped deploy script for new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,47 +1,111 @@
 set :stages, %w(staging production)
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
+require 'capistrano_colors'
 
-# Not needed on new server?
-#default_environment["PATH"] = "/opt/ruby-enterprise/current/bin/:$PATH"
+class Capistrano::ServerDefinition
+  def to_s
+    @to_s ||= begin
+      s = @options[:alias] || host
+      s = "#{user}@#{s}" if user
+      s = "#{s}:#{port}" if port && port != 22
+      s
+    end
+  end
+end
 
 set :application, "purjo"
 set :repository, "git@github.com:datasektionen/purjo.git"
 set :scm, "git"
 
-set :deploy_via, :copy
-set :compression, :gzip
 set :deploy_to, "/var/rails/#{application}" # Will be updated for each stage with stage specific path.
-set :user, "capistrano"
+set :user, "rails"
 set :use_sudo, false
+set :ssh_options, {:forward_agent => true }
 set :rails_env, "migration"
 set :tmp_path, "/var/tmp/rails"
-set :keep_releases, 3
 
-ssh_options[:forward_agent] = true
+set(:latest_release) { fetch(:current_path) }
+set(:release_path) { fetch(:current_path) }
+set(:current_release) { fetch(:current_path) }
 
-#role :app, "mission-to-marzipan.ben-and-jerrys.stacken.kth.se"
-#role :web, "mission-to-marzipan.ben-and-jerrys.stacken.kth.se"
-#role :db,  "mission-to-marzipan.ben-and-jerrys.stacken.kth.se", :primary => true
-role :app, "magic-brownies.ben-and-jerrys.stacken.kth.se"
-role :web, "magic-brownies.ben-and-jerrys.stacken.kth.se"
-role :db,  "magic-brownies.ben-and-jerrys.stacken.kth.se", :primary => true
+set(:current_revision) { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:latest_revision) { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:previous_revision) { capture("cd #{current_path}; git rev-parse --short HEAD@{1}").strip }
+set(:current_revision_info) { capture("cd #{current_path}; git show -s --oneline").strip }
+
+default_environment["RAILS_ENV"] = 'production'
+default_run_options[:shell] = 'bash'
+
+role :app, "clusterfluff.ben-and-jerrys.stacken.kth.se", :alias => "clusterfluff"
+role :web, "clusterfluff.ben-and-jerrys.stacken.kth.se", :alias => "clusterfluff"
+role :db,  "clusterfluff.ben-and-jerrys.stacken.kth.se", :primary => true, :alias => "clusterfluff"
 
 namespace :deploy do
-  #desc "Authenticate using Kerberos"
-  #task :kauth do
-  #  run("kauth -t /home/capistrano/krb5.keytab.capistrano capistrano/haagen-dazs.stacken.kth.se")
-  #end
+  desc "Delpoy"
+  task :default do
+    update
+    restart
+  end
 
-  #desc "Flush authetication tokens"
-  #task :kdestroy do
-  #  run("kdestroy")
-  #end
+  desc "Setup a GitHub-style deployment."
+  task :setup, :except => {:no_release => true} do
+    dirs = [deploy_to, shared_path]
+    dirs += shared_children.map {|d| File.join(shared_path, d)}
+    dir_list = dirs.join(' ')
+    run "#{try_sudo} mkdir -p #{dir_list} && #{try_sudo} chmod g+w #{dir_list}"
+    run "git clone #{repository} #{current_path}"
+  end
+
+  desc "Update the deployed code."
+  task :update_code, :except => {:no_release => true} do
+    run "cd #{current_path}; git checkout .; git pull; git reset --hard #{branch}"
+    finalize_update
+  end
 
   desc "Copy the config files"
   task :update_config do
     run "cp -Rf #{shared_path}/config/* #{release_path}/config/"
     run "ln -sf #{release_path}/config/environments/development.rb #{release_path}/config/environments/migration.rb"
+  end
+
+  desc "Update the database (overridden to avoid symlink)"
+  task :migrations do
+    transaction do
+      update_code
+    end
+    migrate
+    restart
+  end
+
+  desc "Symlink stuff"
+  task :symlink do
+    # nothing to do here, move along
+  end
+
+  desc "Restart the app (kill the unicorns)"
+  task :restart, :except => { :no_release => true } do
+    # we just kill the unicorns, monit will spawn new ones eventually
+    run "kill `cat #{shared_path}/pids/unicorn.pid` || /bin/true"
+  end
+
+  namespace :rollback do
+    desc "Moves the repo back to the previous version of HEAD"
+    task :repo, :except => {:no_release => true} do 
+      set :branch, "HEAD@{1}"
+      deploy.default
+    end
+
+    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
+    task :cleanup, :except => {:no_release => true} do
+      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
+    end
+
+    desc "Rolls back to the previously deployed version."
+    task :default do
+      rollback.repo
+      rollback.cleanup
+    end
   end
 
   desc "Symlink public/file_nodes"
@@ -56,61 +120,42 @@ namespace :deploy do
 
   desc "Symlink tmp"
   task :symlink_tmp do
-    run "rm -rf #{release_path}/tmp"
     run "ln -sf #{tmp_path}/#{application}/#{stage} #{release_path}/tmp"
   end
 
-  desc "Set permissions for public/{stylesheets,javascripts}"
-  task :set_permissions do
-    #run "setfacl -m user:www-data:rwx #{release_path}/public/{stylesheets,javascripts}"
-    #run "setfacl -d -m user:www-data:rwx #{release_path}/public/{stylesheets,javascripts}"
+  desc "Update the crontab file"
+  task :update_crontab, :roles => :db do
+    run "cd #{release_path} && /usr/local/bin/ree_bundle exec whenever --update-crontab #{application}"
   end
-
-  desc "Restarting mod_rails with restart.txt"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
- 
-  [:start, :stop].each do |t|
-    desc "#{t} task is a no-op with mod_rails"
-    task t, :roles => :app do ; end
-  end
-
-  #before "deploy:update_code", "deploy:kauth"
-  #after  "deploy:update_code", "deploy:kdestroy"
-  after  "deploy:update_code", "deploy:update_config"
-  after  "deploy:update_code", "deploy:symlink_file_nodes"
-  after  "deploy:update_code", "deploy:symlink_system"
-  after  "deploy:update_code", "deploy:symlink_tmp"
-  after  "deploy:update_code", "deploy:set_permissions"
-  after  "deploy:update", "deploy:cleanup"
 end
 
 namespace :bundler do
-  namespace :bundler do  
-    task :create_symlink, :roles => :app do
-      set :bundle_dir, 'vendor/bundle'
-      set :shared_bundle_path, File.join(shared_path, 'bundle')
-      
-      run " cd #{release_path} && rm -rf #{bundle_dir}" # in the event it already exists..?
-      run "mkdir -p #{shared_bundle_path} && cd #{release_path} && ln -s #{shared_bundle_path} #{bundle_dir}"
-    end
+  task :create_symlink, :roles => :app do
+    set :bundle_dir, 'vendor/bundle'
+    set :shared_bundle_path, File.join(shared_path, 'bundle')
+
+    run "cd #{release_path} && rm -f #{bundle_dir}" # in the event it already exists..?
+    run "mkdir -p #{shared_bundle_path} && cd #{release_path} && ln -s #{shared_bundle_path} #{bundle_dir}"
   end
 
   task :bundle_new_release, :roles => :app do
     bundler.create_symlink
-    run "cd #{release_path} ; bundle install --path #{shared_bundle_path} --without development"
+    run "cd #{release_path}; /usr/local/bin/ree_bundle install --path #{shared_bundle_path} --without development test"
   end
 end
- 
+
+namespace :stats do
+  desc "print current git revision"
+  task :git_revision, :except => {:no_release => true} do
+    puts current_revision_info
+  end
+end
+
 after 'deploy:update_code', 'bundler:bundle_new_release'
-
-
+after 'deploy:restart', 'stats:git_revision'
+after  "deploy:update_code", "deploy:update_config"
+after  "deploy:update_code", "deploy:symlink_file_nodes"
+after  "deploy:update_code", "deploy:symlink_system"
+after  "deploy:update_code", "deploy:symlink_tmp"
 after "deploy:symlink", "deploy:update_crontab"
 
-namespace :deploy do
-  desc "Update the crontab file"
-  task :update_crontab, :roles => :db do
-    run "cd #{release_path} && bundle exec whenever --update-crontab #{application}"
-  end
-end
