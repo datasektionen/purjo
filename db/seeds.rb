5ce5adfd33968b425adb0608650e9dd73e1cb850
Victor Koronen <koronen@kth.se>
2012-02-08 20:13:26 +0100

    Add required additional_content attribute to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-root_page = TextNode.create!(:name => 'root', :url => '/', :contents => "I is root")
+root_page = TextNode.create!(:name => 'root', :url => '/', :contents => "I is root", :additional_content => "")
 
 person_info_file = Rails.root + "config/my_user.yml"
 raise "Skapa en config/my_user.yml (exempel finns i katalogen)" unless File.exists?(person_info_file)
