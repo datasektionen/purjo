11948e92f42af48b19b2d0fee7e31b569f554220
Martin Frost <frost@ceri.se>
2012-02-29 11:35:16 +0100

    typo in errbit initializer

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |config|
   config.api_key = '76bdad0722032d439cc2553f1928e955'
-  config.host= 'errbit.datasektionen.se'
-  config.port=end 80
-  config.secure= config.port == 443
+  config.host = 'errbit.datasektionen.se'
+  config.port = 80
+  config.secure = config.port == 443
 end
 
