954b64c86e78d8ebf9423a4f9bf006320ca82240
Martin Frost <frost@ceri.se>
2012-08-09 09:47:29 +0200

    Make Morklaggning#username= set person_id. This seems VERY counter-intuitive and wrong.

--- app/models/morklaggning.rb
+++ app/models/morklaggning.rb
@@ -1,11 +1,15 @@
 class Morklaggning < ActiveRecord::Base
   belongs_to :person
 
+  validate :person_id, :presence => true
+
   def username
     self.person.try :kth_username
   end
 
+  # TODO: Revisit, refactor, fix!
+  # This feels just plain wrong.
   def username=(val)
-    self.person = Person.find_by_kth_username(val)
+    self.person_id = Person.find_by_kth_username(val).id
   end
 end
