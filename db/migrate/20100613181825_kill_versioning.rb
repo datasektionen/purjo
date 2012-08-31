1dadd2f37531d5514108a22616ad37c023c9b5bd
Victor Koronen <koronen@kth.se>
2012-08-31 22:35:07 +0200

    Fix KillVersioning migration

--- db/migrate/20100613181825_kill_versioning.rb
+++ db/migrate/20100613181825_kill_versioning.rb
@@ -1,22 +1,9 @@
 class KillVersioning < ActiveRecord::Migration
   def self.up
     drop_table :changes
-    drop_table :text_node_versions
   end
 
   def self.down
-    create_table "text_node_versions", :force => true do |t|
-      t.integer  "text_node_id"
-      t.integer  "version"
-      t.integer  "parent_id"
-      t.string   "name"
-      t.string   "url"
-      t.text     "contents"
-      t.datetime "created_at"
-      t.datetime "updated_at"
-      t.string   "custom_layout"
-    end
-    
     create_table "changes", :force => true do |t|
       t.string   "action"
       t.integer  "changed_object_id"
@@ -25,6 +12,5 @@ class KillVersioning < ActiveRecord::Migration
       t.datetime "created_at"
       t.datetime "updated_at"
     end
-    
   end
 end
