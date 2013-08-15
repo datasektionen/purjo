c749167533d260d94946380da63b4b86eeb2a6bb
Victor Koronen <koronen@kth.se>
2013-08-15 12:29:33 +0200

    Default to reading the secret token from the environment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,7 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = '8f53924a27ba15125e9242cea92fe326d4c31fe1ec80550836290a364a6004032efcd3566270aadf9b8b20a3e9d9e86a934ec09bbce560153503051bd38f425a'
+Rails.application.config.secret_token = ENV["SECRET_TOKEN"] ||
+  '8f53924a27ba15125e9242cea92fe326d4c31fe1ec80550836290a364a6004032efcd3566270aadf9b8b20a3e9d9e86a934ec09bbce560153503051bd38f425a'
