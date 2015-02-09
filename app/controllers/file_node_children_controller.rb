5c1394562ea00d6c95b4dc44895bbc7a0d44e5a4
viklu <viklu@kth.se>
2015-02-09 16:39:22 +0100

    Add functionality to upload multiple files at once

--- app/controllers/file_node_children_controller.rb
+++ app/controllers/file_node_children_controller.rb
@@ -10,14 +10,16 @@ class FileNodeChildrenController < ApplicationController
   
   def create
     @parent = TextNode.find(params[:text_node_id])
-    @node = @parent.file_nodes.build(params[:file_node])
-    
-    if @node.save
-      flash[:notice] = "Fil uppladdad"
-      redirect_to @parent.url
-    else
-      flash[:error] = "Ett fel uppstod vid uppladdning av fil"
-      render :action => 'edit'
+    params[:file_node][:resource].each do |file_node|
+      @node = @parent.file_nodes.build({:resource => file_node})
+      unless @node.save
+        flash[:error] = "Ett fel uppstod vid uppladdning av fil"
+        render :action => 'new'
+	return
+      end
     end
+
+    flash[:notice] = "Fil uppladdad"
+    redirect_to @parent.url
   end
 end
