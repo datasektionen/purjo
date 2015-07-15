c7caa4d198d659b1fc30bbd97fde2743683b2f0c
Martin Frost <frost@ceri.se>
2015-07-15 22:45:12 +0200

    added AnonymousPerson#editor?

--- app/models/anonymous_person.rb
+++ app/models/anonymous_person.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
 class AnonymousPerson
   extend ActiveModel::Naming
-  
+
   def first_name
     "Anonym"
   end
-  
+
   def last_name
     "Användare"
   end
@@ -13,30 +13,34 @@ class AnonymousPerson
   def name
     first_name
   end
-  
+
   def to_s
     "Anonym"
   end
-  
+
   def id
     nil
   end
-  
+
   # Use some dynamic programming voodo for syntactic sugar, again, DRY IT UP!
   Role.all.each do |role|
-    define_method "#{role.to_s}?" do 
+    define_method "#{role.to_s}?" do
       false
     end
   end
-  
+
   def has_role?(role)
     false
   end
-  
+
   def anonymous?
     true
   end
-  
+
+  def editor?
+    false
+  end
+
   def has_feature?(feature)
     false
   end
