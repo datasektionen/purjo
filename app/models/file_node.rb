be7e775d3e296af41177aa0dc9b42f104b740166
Jesper Bränn <brann.jesper@gmail.com>
2015-07-26 13:47:35 +0200

    Do not validate file nodes (new in paperclip > 4)

--- app/models/file_node.rb
+++ app/models/file_node.rb
@@ -1,42 +1,43 @@
 # encoding: utf-8
 class FileNode < ActiveRecord::Base
   has_attached_file :resource, :path => ':rails_root/public/file_nodes/:parent_id_partition/:filename'
-  
+  do_not_validate_attachment_file_type :resource
+
   belongs_to :parent, :class_name => 'TextNode', :foreign_key => 'parent_id'
-  
+
   before_validation :update_url
-  
+
   def name
     resource_file_name
   end
-  
+
   protected
   def update_url
     node = self
-    
+
     if node.parent.nil?
       self.url = ""
       return
     end
-    
+
     if self.name.nil?
       errors.add "name", "must be set"
       return
     end
-    
+
     url_parts = [self.name]
-    
+
     # Låt oss inte skriva ut namnet på root-noden
     while node.parent != nil and node.parent.parent != nil
       url_parts << node.parent.name
       node = node.parent
     end
-    
+
     sanitized_parts = Node.sanitize_url(url_parts)
     url = "/" + sanitized_parts.reverse.join("/")
-    
+
     self.url =  url
-    
+
   end
-  
+
 end
